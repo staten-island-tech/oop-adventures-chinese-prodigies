b993541539605b7d2be7f52663292e301a53e3eb
JustinStrawberries <Justinl870@nycstudents.net>
2026-05-08 10:11:49 -0400

    iuhewoijw

--- People.py
+++
@@ -1,9 +0,0 @@
-# This is going to have classes of people like soldiers, farmers, civilians, etc
-class Roles:
-    def __init__(self, health, inventory):
-        self.health = health
-        self.inventory = inventory
-
-class Civilians(Roles):
-    def __init__(self, inventory):
-        super().__init__(75, inventory)
