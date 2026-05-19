9c67fe1be94d2d228d9dd08764bd0f23d95274e1
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-05-19 10:15:51 -0400

    sx

--- day.py
+++ day.py
@@ -3,7 +3,7 @@ import random
 def day(self):
     self.daynumber += 1
     print(f"\nA new day begins... Day {self.daynumber}")
-    consumed = self.population * random.randint(1, 5)
+    consumed = self.population * random.randint(1, 2)+23
     self.food -= consumed
     print(f"\n Your population of {self.population} has consumed {consumed} rice bowls")
 
